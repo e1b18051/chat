aa9a4937df8d1aef8d2da3e6ac6ca33cede15d4a
e1b18075 <e1b18075@oit.ac.jp>
2020-11-13 12:17:19 +0900

    home.htmlの変更(入出力、ログアウト)

--- src/main/java/oit/is/group8/chat/controller/ChatController.java
+++ src/main/java/oit/is/group8/chat/controller/ChatController.java
@@ -5,9 +5,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/home")
 public class ChatController {
 
   /**
@@ -15,9 +17,14 @@ public class ChatController {
    *
    * @return
    */
-  @GetMapping("/home")
+  @GetMapping("/output")
+  public String home(@RequestParam String name, ModelMap model) {
+    model.addAttribute("name", name);
+    return "home.html";
+
+  }
+  @GetMapping("")
   public String home() {
     return "home.html";
   }
-
 }
